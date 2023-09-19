e562afd0ca9abe60f454c85591961c8e2a3c1ff3
Iamneo <techadmin@iamneo.ai>
2023-09-19 15:33:44 +0000

    Backup Commit

--- Project/src/test/java/runner/Testcase1.java
+++ Project/src/test/java/runner/Testcase1.java
@@ -47,51 +47,51 @@ public class Testcase1 extends Base {
     //     }
     // }
 
-     @Test(priority = 2)
-    public void TC_002() throws IOException {
-        try {
-            ExtentTest test = reporter.createTest("Register Test", "Execution for Register Function");
-            driver.get(prop.getProperty("url"));
-            Homepage.Valid_Login_TC(driver);
-            deposit.Deposit_Amount(driver);
-            test.log(Status.PASS, "Register Test");
-        } 
-        catch (Exception ex) {
-            ex.printStackTrace();
-            Screenshot.getScreenShot(driver, "Register_test");
-            ExtentTest test = reporter.createTest("Register Test");
-            String base64Screenshot = screenshotHandler.captureScreenshotAsBase64(driver);
-            test.log(Status.FAIL, "Unable to Perform Register Test", MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
-           
-        }
-    }
-
-
-
-
-
-
-
-    // @Test(priority = 3)
-    // public void TC_003() throws IOException {
+    //  @Test(priority = 2)
+    // public void TC_002() throws IOException {
     //     try {
-    //         ExtentTest test = reporter.createTest("WithdrawTest ", "Execution for WithdrawTest");
+    //         ExtentTest test = reporter.createTest("Register Test", "Execution for Register Function");
     //         driver.get(prop.getProperty("url"));
     //         Homepage.Valid_Login_TC(driver);
-    //         withdraw.WithdrawTest(driver);
-
-    //         String base64Screenshot = screenshotHandler.captureScreenshotAsBase64(driver);
-    //         test.log(Status.PASS, "WithdrawTest", MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
-
-    //     }catch (Exception ex) {
+    //         deposit.Deposit_Amount(driver);
+    //         test.log(Status.PASS, "Register Test");
+    //     } 
+    //     catch (Exception ex) {
     //         ex.printStackTrace();
-    //         Screenshot.getScreenShot(driver, "Withdraw test");
-    //         ExtentTest test = reporter.createTest("WithdrawTest");
+    //         Screenshot.getScreenShot(driver, "Register_test");
+    //         ExtentTest test = reporter.createTest("Register Test");
     //         String base64Screenshot = screenshotHandler.captureScreenshotAsBase64(driver);
-    //         test.log(Status.FAIL, "Unable to Perform WithdrawTest ", MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
+    //         test.log(Status.FAIL, "Unable to Perform Register Test", MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
            
     //     }
     // }
+
+
+
+
+
+
+
+    @Test(priority = 3)
+    public void TC_003() throws IOException {
+        try {
+            ExtentTest test = reporter.createTest("WithdrawTest ", "Execution for WithdrawTest");
+            driver.get(prop.getProperty("url"));
+            Homepage.Valid_Login_TC(driver);
+            withdraw.WithdrawTest(driver);
+
+            String base64Screenshot = screenshotHandler.captureScreenshotAsBase64(driver);
+            test.log(Status.PASS, "WithdrawTest", MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
+
+        }catch (Exception ex) {
+            ex.printStackTrace();
+            Screenshot.getScreenShot(driver, "Withdraw test");
+            ExtentTest test = reporter.createTest("WithdrawTest");
+            String base64Screenshot = screenshotHandler.captureScreenshotAsBase64(driver);
+            test.log(Status.FAIL, "Unable to Perform WithdrawTest ", MediaEntityBuilder.createScreenCaptureFromBase64String(base64Screenshot).build());
+           
+        }
+    }
     
     
 
